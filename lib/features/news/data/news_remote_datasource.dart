d95d2201fff5020083a36f5012a8b6d60f34ca1d
Asif-Faizal <moh.asif@protonmail.ch>
2024-12-27 14:44:40 +0530

    API KEY

--- lib/features/news/data/news_remote_datasource.dart
+++ lib/features/news/data/news_remote_datasource.dart
@@ -1,8 +1,8 @@
 import 'package:tdd_clean/features/news/data/news_model.dart';
 
 abstract class NewsRemoteDatasource {
-  // calls API [https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d26344a4cc7045a895af69f018609a64]
+  // calls API [https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=API_KEY]
   Future<NewsModel> getQueryNews(String query);
-  // calls API [https://newsapi.org/v2/everything?q=apple&sortBy=publishedAt&apiKey=d26344a4cc7045a895af69f018609a64]
+  // calls API [https://newsapi.org/v2/everything?q=apple&sortBy=publishedAt&apiKey=API_KEY]
   Future<NewsModel> getCountryNews(String country, String category);
 }
