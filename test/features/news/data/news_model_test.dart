130d79de17448427e08889837847544803879597
Asif-Faizal <moh.asif@protonmail.ch>
2024-12-27 11:30:25 +0530

    MODEL FROM JSON

--- test/features/news/data/news_model_test.dart
+++ test/features/news/data/news_model_test.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tdd_clean/features/news/data/news_model.dart';
 import 'package:tdd_clean/features/news/domain/news_entity.dart';
+
+import '../../../fixtures/fixture_reader.dart';
 
 void main() {
   group('NewsModel tests', () {
@@ -18,7 +22,7 @@ void main() {
       content: null,
     );
     final tNewsModel2 = NewsModel(
-      sourceId: '16723541',
+      sourceId: '28734685',
       sourceName: 'Reuters',
       author: 'Reuters',
       title:
@@ -28,7 +32,8 @@ void main() {
           'https://www.reuters.com/world/asia-pacific/passenger-plane-crashes-kazakhstan-emergencies-ministry-says-2024-12-25/',
       urlToImage: 'http://example.com',
       publishedAt: DateTime.parse('2024-12-25T08:19:37Z'),
-      content: 'Passenger plane flying from Azerbaijan to Russia crashes in Kazakhstan with many feared dead - Reuters',
+      content:
+          'Passenger plane flying from Azerbaijan to Russia crashes in Kazakhstan with many feared dead - Reuters',
     );
 
     test('Should be a subclass of NewsEntity for nullable response', () async {
@@ -36,9 +41,30 @@ void main() {
       expect(tNewsModel1, isA<NewsEntity>());
     });
 
-    test('Should be a subclass of NewsEntity for non-nullable response', () async {
+    test('Should be a subclass of NewsEntity for non-nullable response',
+        () async {
       // arrange
       expect(tNewsModel2, isA<NewsEntity>());
+    });
+
+    test('fromJson method for nullable response', () async {
+      //arrange
+      final Map<String, dynamic> jsonMap =
+          json.decode(fixture('news_null.json'));
+      //act
+      final result = NewsModel.fromJson(jsonMap);
+      //assert
+      expect(result, tNewsModel1);
+    });
+
+    test('fromJson method for non nullable response', () async {
+      //arrange
+      final Map<String, dynamic> jsonMap =
+          json.decode(fixture('news_non_null.json'));
+      //act
+      final result = NewsModel.fromJson(jsonMap);
+      //assert
+      expect(result, tNewsModel2);
     });
   });
 }
